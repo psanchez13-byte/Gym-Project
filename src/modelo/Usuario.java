346d738c441339ab419e30a4633f77a0a5741d16
Pedro Pablo Sanchez Nualart <p.sanchez13@ufromail.cl>
2026-09-26 00:50:47 -0300

    Se elimino el main sobrante y se añadio varaiables extras a Usuario

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -10,33 +10,70 @@ public class Usuario {
     private double altura;
     private double peso;
     private String genero; //F O M
-    private int diasactivofis;
+    private int diasActivoFis;
     // DATOS OBLIGATORISO DEL TEST
 
     public Usuario(String nombre, String matricula, String contraseña) {
         this.nombre = nombre;
-        this.matricula= matricula;
+        this.matricula = matricula;
         this.contraseña = contraseña;
 
 
     }
 
+    // Getters
     public String getNombre() {
         return nombre;
     }
 
-    public String getMatricula(){
+    public String getMatricula() {
         return matricula;
     }
 
-    public String getContraseña(){
+    public String getContraseña() {
         return contraseña;
     }
 
+    // Setters y Getters para los datos del test
 
+    public int getEdad() {
+        return edad;
+    }
 
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
+    public double getAltura() {
+        return altura;
+    }
 
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDiasActivoFis() {
+        return diasActivoFis;
+    }
+
+    public void setDiasActivoFis(int diasActivoFis) {
+        this.diasActivoFis = diasActivoFis;
+    }
 
 }
